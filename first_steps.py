429765d020ea9631a84e2eb0b74cecb169ef2925
Raul Campa <raulc@escape-technology.com>
2024-03-30 22:55:17 +0000

    created function to append tracks to a playlist

--- first_steps.py
+++ first_steps.py
@@ -5,6 +5,7 @@ import requests
 import os
 import json
 import base64
+import math
 
 class Spotify_App():
 
@@ -288,38 +289,7 @@ class Spotify_App():
             return r.json()
         else:
             self.error_message(r)
-        
-    # # this is being held up by not being able to create playlists
-    # def append_tracks_to_playlist(self, playlist_id, uris, duplicates=False):
-    #     if self.client_token_expired():
-    #         self.get_client_auth_token()
-    #     payload = {
-    #         "Authorization": f"Bearer {self.client_auth_token}",
-    #         "scope": "playlist-modify-private",
-    #         "uris": []
-    #     }
-    #     for uri in uris:
-    #         if duplicates:
-    #             if uri.find("spotify:track:") == -1:
-    #                 payload["uris"].append(f"spotify:track:{uri}")
-    #             else:
-    #                 payload["uris"].append(uri)
-    #         else:
-    #             if uri.find("spotify:track:") == -1:
-    #                 uri_formatted = f"spotify:track:{uri}"
-    #             else:
-    #                 uri_formatted = uri
 
-    #             if uri_formatted not in self.get_uris(playlist_id):
-    #                 payload["uris"].append(uri_formatted)
-    #     print(payload)
-    #     r = requests.post(f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks", data=payload)
-    #     if r.status_code != 201:
-    #         print("##################################################")
-    #         print("This request has resulted in a unexpected response\nThe response status code was: " + str(r.status_code))
-    #         print("##################################################")
-    #     else:
-    #         return self.get_playlist_tracks(playlist_id)
     def created_playlist_cleanup(self):
         with open(f"app_info/created_playlists_{self.name}.json", "r") as file:
             playlists_dict = json.loads(file.read())
@@ -330,6 +300,71 @@ class Spotify_App():
         with open(f"app_info/created_playlists_{self.name}.json", "w") as file:
             file.write(playlists_json)
         return playlists_json
+    
+    def append_tracks_to_playlist(self, playlist_id, uris, duplicates=False):
+        self.read_user_auth_token
+        if self.user_auth_token_expired():
+            self.user_auth_token_from_refresh_token()
+        hdrs = {
+            "Authorization": f"Bearer {self.user_auth_token}",
+            "Content-Type": "application/json"
+        }
+        tracks_to_append = []
+        for uri in uris:
+            if duplicates:
+                if uri.find("spotify:track:") == -1:
+                    tracks_to_append.append(f"spotify:track:{uri}")
+                else:
+                    tracks_to_append.append(uri)
+            else:
+                if uri.find("spotify:track:") == -1:
+                    uri_formatted = f"spotify:track:{uri}"
+                else:
+                    uri_formatted = uri
+
+                if uri_formatted not in self.get_playlist_uris(playlist_id):
+                    tracks_to_append.append(uri_formatted)
+        # you can only add 100 items per request https://developer.spotify.com/documentation/web-api/reference/add-tracks-to-playlist
+        if len(tracks_to_append) == 0:
+            print("#########################################################")
+            print("After removing duplicates there are no tracks left to add")
+            print("#########################################################")
+        elif len(tracks_to_append) <= 100:
+            payload = {
+                "uris": tracks_to_append
+            }
+            payload_json = json.dumps(payload)
+            r = requests.post(f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks", headers=hdrs, data=payload_json)
+            if self.request_successful(r):
+                return r.json()
+            else:
+                self.error_message(r)
+        else:
+            slices = math.ceil(len(tracks_to_append) / 100)
+            print(slices)
+            for n in range(1, (slices)):
+                lower_slice = 0 + (100 * (n - 1))
+                upper_slice = 100 + (100 * (n - 1))
+                payload = {
+                    "scope": "playlist-modify-private",
+                    "uris": tracks_to_append[lower_slice:upper_slice]
+                }
+                r = requests.post(f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks", headers=hdrs, data=payload)
+                if self.request_successful(r):
+                    return r.json()
+                else:
+                    self.error_message(r)
+            final_lower = 100 * (slices - 1)
+            payload = {
+                "uris": tracks_to_append[final_lower::]
+            }
+            r = requests.post(f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks", headers=hdrs, data=payload)
+            if self.request_successful(r):
+                return r.json()
+            else:
+                self.error_message(r)
+                
+
 
 
 first_app = Spotify_App("66768cc0e0fb4cc5a9ae5421aa6c399c", "644255b4d32e4644a3b25009a35b0dfb")
