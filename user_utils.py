f356b7746bd6b8f5a67a10e75a0cf955cc9367e0
Raul Campa <raulc@escape-technology.com>
2024-04-22 18:02:50 +0100

    refactored user_utils.py

--- user_utils.py
+++ user_utils.py
@@ -1,23 +1,22 @@
 import requests
-import json
 from response_utils import Response
+from user_auth_utils import UserAuth
 
 class User():
-    def __init__(self, user_auth_token, user_refresh_token):
+    def __init__(self, name, user_auth_token, user_refresh_token, client_id, client_secret):
+        self.name = name
         self.user_auth_token = user_auth_token
         self.user_refresh_token = user_refresh_token
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.UserAuth = UserAuth(self.name, self.user_auth_token, self.user_refresh_token, self.client_id, self.client_secret)
 
-    def read_user_auth_token(self):
-        with open(f"app_info/{self.name}.json", 'r') as file:
-            read_dict = json.loads(file.read())
-        self.user_auth_token = read_dict["user_auth_token"]
-
-    def get_user_info(self, user_auth_token):
-        self.read_user_auth_token(user_auth_token)
-        if self.user_auth_token_expired(user_auth_token):
-            self.user_auth_token_from_refresh_token(user_auth_token)
+    def get_user_info(self):
+        self.UserAuth.read_user_auth_token()
+        if self.UserAuth.user_auth_token_expired():
+            self.UserAuth.user_auth_token_from_refresh_token()
         hdrs = {
-            "Authorization": f"Bearer {user_auth_token}",
+            "Authorization": f"Bearer {self.user_auth_token}",
             "scope": "user-read-private"
         }
         r = requests.get('https://api.spotify.com/v1/me', headers=hdrs)
