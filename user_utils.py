9c607dc1c0a39479630b1955f93488fc988de466
Raul Campa <raulc@escape-technology.com>
2024-04-23 13:12:24 +0100

    refactored user_utils.py
    removed class instantiation from __init__()

--- user_utils.py
+++ user_utils.py
@@ -1,22 +1,16 @@
 import requests
 from response_utils import Response
-from user_auth_utils import UserAuth
 
 class User():
-    def __init__(self, name, user_auth_token, user_refresh_token, client_id, client_secret):
-        self.name = name
-        self.user_auth_token = user_auth_token
-        self.user_refresh_token = user_refresh_token
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.UserAuth = UserAuth(self.name, self.user_auth_token, self.user_refresh_token, self.client_id, self.client_secret)
+    def __init__(self, userauth):
+        self.userauth = userauth
 
     def get_user_info(self):
-        self.UserAuth.read_user_auth_token()
-        if self.UserAuth.user_auth_token_expired():
-            self.UserAuth.user_auth_token_from_refresh_token()
+        self.userauth.read_user_auth_token()
+        if self.userauth.user_auth_token_expired():
+            self.userauth.user_auth_token_from_refresh_token()
         hdrs = {
-            "Authorization": f"Bearer {self.user_auth_token}",
+            "Authorization": f"Bearer {self.userauth.return_user_auth_token()}",
             "scope": "user-read-private"
         }
         r = requests.get('https://api.spotify.com/v1/me', headers=hdrs)
