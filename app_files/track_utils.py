b64b911a171dad8c34c71d175324f40c11bc6427
Raul Campa <raulc@escape-technology.com>
2024-05-03 14:10:01 +0100

    created get_audio_features and get_audio_analysis

--- app_files/track_utils.py
+++ app_files/track_utils.py
@@ -20,3 +20,25 @@ class Track():
             return r.json()
         else:
             Response.error_message(r)
+
+    def get_audio_features(self, track_id):
+        self.manage_client_creds()
+        hdrs = {
+            "Authorization": f"Bearer {self.client.return_client_auth_token()}"
+        }
+        r = requests.get(f"https://api.spotify.com/v1/audio-features/{track_id}", headers=hdrs)
+        if Response.request_successful(r):
+            return r.json()
+        else:
+            Response.error_message(r)
+
+    def get_audio_analysis(self, track_id):
+        self.manage_client_creds()
+        hdrs = {
+            "Authorization": f"Bearer {self.client.return_client_auth_token()}"
+        }
+        r = requests.get(f"https://api.spotify.com/v1/audio-analysis/{track_id}", headers=hdrs)
+        if Response.request_successful(r):
+            return r.json()
+        else:
+            Response.error_message(r)
